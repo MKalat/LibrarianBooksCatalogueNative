cb966d79a28727517bedba82c5d2d75eedd3b5ff
MarcinKałat <fixer44@wp.pl>
2014-06-06 10:46:31 +0200

    dodane tłumaczenia

---
+++ LibrarianBooksCatalogue/librarianbookscatalogue_en.ts
@@ -0,0 +1,346 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL" sourcelanguage="en">
+<context>
+    <name>LibrarianBooksCatalogue</name>
+    <message>
+        <location filename="lbookscatalogue.ui" line="29"/>
+        <source>Librarian Books Catalogue</source>
+        <translation>Librarian Books Catalogue</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="45"/>
+        <source>Title</source>
+        <translation>Tytuł</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="68"/>
+        <source>Orig. title</source>
+        <translation>Tytuł oryginału</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="91"/>
+        <source>Genre</source>
+        <translation>Gatunek</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="114"/>
+        <source>Amount</source>
+        <translation>Ilość</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="137"/>
+        <location filename="lbookscatalogue.ui" line="387"/>
+        <source>Date Pub.</source>
+        <translation>Data Wyd.</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="160"/>
+        <location filename="lbookscatalogue.ui" line="392"/>
+        <source>Publisher</source>
+        <translation>Wydawca</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="183"/>
+        <location filename="lbookscatalogue.ui" line="397"/>
+        <source>Pub. Lang.</source>
+        <translation>Jęz. Wyd.</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="213"/>
+        <source>Description</source>
+        <translation>Opis</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="244"/>
+        <source>Decription</source>
+        <translation>Opis</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="267"/>
+        <location filename="lbookscatalogue.ui" line="377"/>
+        <location filename="lbookscatalogue.ui" line="767"/>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="272"/>
+        <location filename="lbookscatalogue.ui" line="382"/>
+        <location filename="lbookscatalogue.ui" line="772"/>
+        <source>IDM</source>
+        <translation>IDM</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="277"/>
+        <location filename="lbookscatalogue.ui" line="490"/>
+        <source>Name &amp; Lastname</source>
+        <translation>Imie i Nazwisko</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="282"/>
+        <source>Nationality</source>
+        <translation>Narodowość</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="287"/>
+        <source>Interests</source>
+        <translation>Zainteresowania</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="292"/>
+        <source>Chapters</source>
+        <translation>Rozdziały</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="306"/>
+        <source>Authors</source>
+        <translation>Autorzy</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="319"/>
+        <location filename="lbookscatalogue.ui" line="434"/>
+        <location filename="lbookscatalogue.ui" line="824"/>
+        <location filename="lbookscatalogue.ui" line="1057"/>
+        <source>Delete</source>
+        <translation>Usuń</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="332"/>
+        <location filename="lbookscatalogue.ui" line="421"/>
+        <location filename="lbookscatalogue.ui" line="811"/>
+        <location filename="lbookscatalogue.ui" line="1016"/>
+        <source>New</source>
+        <translation>Nowy</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="345"/>
+        <source>Editions</source>
+        <translation>Wydania</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="402"/>
+        <source>Edition No.</source>
+        <translation>Nr Wydania</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="407"/>
+        <source>Edition Country</source>
+        <translation>Kraj Wydania</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="447"/>
+        <location filename="lbookscatalogue.ui" line="460"/>
+        <location filename="lbookscatalogue.ui" line="837"/>
+        <location filename="lbookscatalogue.ui" line="1035"/>
+        <source>Save</source>
+        <translation>Zapisz</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="466"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="478"/>
+        <source>Owner Data</source>
+        <translation>Dane posiadacza</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="513"/>
+        <location filename="lbookscatalogue.ui" line="572"/>
+        <source>Adress</source>
+        <translation>Adres</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="537"/>
+        <source>Shop Data</source>
+        <translation>Dane sklepu</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="549"/>
+        <source>Name</source>
+        <translation>Imię</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="595"/>
+        <source>WWW</source>
+        <translation>WWW</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="619"/>
+        <source>Additional Info</source>
+        <translation>Dodatkowe informacje</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="631"/>
+        <source>Price</source>
+        <translation>Cena</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="644"/>
+        <source>Pages count</source>
+        <translation>Liczba stron</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="657"/>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="670"/>
+        <source>Binding</source>
+        <translation>Oprawa</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="717"/>
+        <source>Librarian</source>
+        <translation>Bibliotekarz</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="729"/>
+        <source>Borrows</source>
+        <translation>Wypożyczenia</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="777"/>
+        <source>Person</source>
+        <translation>Osoba</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="782"/>
+        <source>Date borrowed</source>
+        <translation>Data wypozyczenia</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="787"/>
+        <source>Date of return</source>
+        <translation>Data zwrotu</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="792"/>
+        <source>Condition before borrowing</source>
+        <translation>Stan przed wypożyczeniem</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="797"/>
+        <source>Condition after return</source>
+        <translation>Stan po oddaniu</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="852"/>
+        <source>Record</source>
+        <translation>Rekord</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="890"/>
+        <source>of</source>
+        <translation>z</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="931"/>
+        <source>|&lt;</source>
+        <comment>|&lt;</comment>
+        <extracomment>|&lt;</extracomment>
+        <translation>|&lt;</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="959"/>
+        <source>&lt;</source>
+        <translation>&lt;</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="978"/>
+        <source>&gt;</source>
+        <translation>&gt;</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="997"/>
+        <source>&gt;|</source>
+        <translation>&gt;|</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="1072"/>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="1080"/>
+        <location filename="lbookscatalogue.ui" line="1118"/>
+        <source>Help</source>
+        <translation>Pomoc</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="1098"/>
+        <source>Search</source>
+        <translation>Szukaj</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="1103"/>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="1108"/>
+        <source>Exit</source>
+        <translation>Koniec</translation>
+    </message>
+    <message>
+        <location filename="lbookscatalogue.ui" line="1113"/>
+        <source>About</source>
+        <translation>O Programie</translation>
+    </message>
+</context>
+<context>
+    <name>Wyszukaj</name>
+    <message>
+        <location filename="Wyszukaj.ui" line="20"/>
+        <source>LBC - Search</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="38"/>
+        <source>Search</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="71"/>
+        <source>Search in field</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="91"/>
+        <source>Title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="96"/>
+        <source>Orig. Title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="101"/>
+        <source>Genre</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="106"/>
+        <source>Date Pub.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="111"/>
+        <source>Authors</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="131"/>
+        <source>S E A R C H</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Wyszukaj.ui" line="144"/>
+        <source>Results</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
