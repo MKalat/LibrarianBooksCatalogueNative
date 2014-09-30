2335213bfc35e17e90710919c231baf56e0bc3b7
MarcinKałat <fixer44@wp.pl>
2014-09-30 17:29:26 +0200

    printWiz added code

--- LibrarianBooksCatalogue/Drukuj.h
+++ LibrarianBooksCatalogue/Drukuj.h
@@ -3,14 +3,22 @@
 
 #include <QtGui/QDialog>
 #include "ui_Drukuj.h"
+#include <QMessageBox>
+#include <QStringList>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
 
 class Drukuj : public QDialog
 {
 	Q_OBJECT
 
 public:
-	Drukuj(QWidget *parent = 0, Qt::WFlags flags = 0);
+    Drukuj(QWidget *parent = 0, Qt::WFlags flags = 0, int id_act = 0);
 	~Drukuj();
+	int id;
+
 	
 
 public slots:
